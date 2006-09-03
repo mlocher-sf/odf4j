db65ab8d0edc4d36b0fa22059aedf430b5f71214
Michael Locher <xray@trash.net>
2006-09-03 09:34:11 +0000

    create maven modules, step 7

--- odf4j/odf4j-packaging/src/test/java/net/sf/odf4j/pkg/SchemaTest.java
+++ odf4j/odf4j-packaging/src/test/java/net/sf/odf4j/pkg/SchemaTest.java
@@ -1,6 +1,6 @@
 // odf4j: OASIS Open Document Library for Java.
 // Copyright (C) 2006 Michael Locher <michael.locher@acm.org>
-package net.sf.odf4j.schema;
+package net.sf.odf4j.pkg;
 
 import java.io.InputStream;
 
